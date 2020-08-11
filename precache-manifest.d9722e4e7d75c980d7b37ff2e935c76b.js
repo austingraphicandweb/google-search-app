6b6678234376add48e1c4a3db598588d2302bfc3
Austin Keener <austingraphicandweb@gmail.com>
2020-08-10 21:27:20 -0400

    Updates

---
+++ precache-manifest.d9722e4e7d75c980d7b37ff2e935c76b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c26665eb2078ea8aaa627ec62bd32a1",
+    "url": "/google-search-app/index.html"
+  },
+  {
+    "revision": "cff858fca78ce9166376",
+    "url": "/google-search-app/static/css/main.00550d83.chunk.css"
+  },
+  {
+    "revision": "f784dfe8fbdf1dfb29b7",
+    "url": "/google-search-app/static/js/2.453bc1fd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google-search-app/static/js/2.453bc1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff858fca78ce9166376",
+    "url": "/google-search-app/static/js/main.1af6ff20.chunk.js"
+  },
+  {
+    "revision": "f107113eeb41ec26a538",
+    "url": "/google-search-app/static/js/runtime-main.c0e4cd88.js"
+  }
+]);
