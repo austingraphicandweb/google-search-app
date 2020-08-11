d5114ae24788608f753789fda482e63b228302be
Austin Keener <austingraphicandweb@gmail.com>
2020-08-10 21:19:45 -0400

    fixing ghpages issue

---
+++ src/pages/search/search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../search/search.css'
+
+function Search() {
+        return (
+            <div className="searchForm">
+               Search form placeholder
+            </div>
+        )
+}
+
+export default Search;
